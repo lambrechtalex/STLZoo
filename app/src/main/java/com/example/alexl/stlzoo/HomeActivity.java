8e11cbe1784bd1968b84b56feb3f5a67f3fbc00f
samikshasm <mailarpwarss@slu.edu>
2018-02-12 13:14:54 -0600

    fixed errors, deleted google-services.json

--- app/src/main/java/com/example/alexl/stlzoo/HomeActivity.java
+++ app/src/main/java/com/example/alexl/stlzoo/HomeActivity.java
@@ -9,16 +9,13 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 import com.google.android.gms.appindexing.AppIndex;
@@ -31,8 +28,6 @@ import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.LocationSettingsResult;
 import com.google.android.gms.location.LocationSettingsStatusCodes;
-
-import static android.media.MediaRecorder.VideoSource.CAMERA;
 
 public class HomeActivity extends AppCompatActivity {
 
