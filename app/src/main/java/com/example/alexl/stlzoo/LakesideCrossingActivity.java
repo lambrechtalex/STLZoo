a661536a9d42e9463c7da561710970ce69f98b8d
samikshasm <mailarpwarss@slu.edu>
2018-03-29 11:50:46 -0500

    Navigation drawer + all the region/dining activities

---
+++ app/src/main/java/com/example/alexl/stlzoo/LakesideCrossingActivity.java
@@ -0,0 +1,265 @@
+package com.example.alexl.stlzoo;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.ListActivity;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.TextView;
+
+/**
+ * Created by samikshasm on 3/28/18.
+ */
+
+public class LakesideCrossingActivity extends ListActivity {
+
+    // Progress Dialog
+    private ProgressDialog pDialog;
+
+    // Creating JSON Parser object
+    JSONParser jParser = new JSONParser();
+
+    ArrayList<HashMap<String, String>> animalsList;
+
+    // url to get all products list
+    private static String url_all_animals = "http://franzcars.mynetgear.com:443/sec5Animals.php";
+
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+    private static final String TAG_ANIMALS = "animals";
+    private static final String TAG_ANIMAL_ID = "Animal_ID";
+    private static final String TAG_NAME = "Common_name";
+
+    private DrawerLayout mDrawer;
+    private Toolbar toolbar;
+    private NavigationView nvView;
+
+    // products JSONArray
+    JSONArray animals = null;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.all_animals);
+
+        // Hashmap for ListView
+        animalsList = new ArrayList<HashMap<String, String>>();
+
+        // Loading products in Background Thread
+        new LoadAllAnimals().execute();
+
+        // Get listview
+        ListView lv = getListView();
+
+        // on seleting single product
+        // launching Edit Product Screen
+        lv.setOnItemClickListener(new OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // getting values from selected ListItem
+                String Animal_ID = ((TextView) view.findViewById(R.id.Animal_ID)).getText()
+                        .toString();
+
+                // Starting new intent
+                //Intent in = new Intent(getApplicationContext(),
+                //EditProductActivity.class);
+                // sending pid to next activity
+                //in.putExtra(TAG_ANIMAL_ID, Animal_ID);
+
+                // starting new activity and expecting some response back
+                //startActivityForResult(in, 100);
+            }
+        });
+
+        // Set a Toolbar to replace the ActionBar.
+        /*toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);*/
+
+        // Find our drawer view
+        mDrawer = findViewById(R.id.drawer_layout);
+
+        nvView = findViewById(R.id.nvView);
+
+        nvView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+
+            // This method will trigger on item Click of navigation menu
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                if(menuItem.getItemId() == R.id.rivers_edge){
+                    Log.d("Entered case 1", "Entered case1");
+                    Intent riverAct = new Intent(getApplicationContext(), RiversEdgeActivity.class);
+                    startActivity(riverAct);
+                }
+                if(menuItem.getItemId() == R.id.the_wild) {
+                    Intent wildAct = new Intent(getApplicationContext(), TheWildActivity.class);
+                    startActivity(wildAct);
+                }
+                if(menuItem.getItemId() == R.id.discovery_center) {
+                    Intent discoveryAct = new Intent(getApplicationContext(), DiscoveryCenterActivity.class);
+                    startActivity(discoveryAct);
+                }
+                if(menuItem.getItemId() == R.id.historic_hill) {
+                    Intent historicAct = new Intent(getApplicationContext(), HistoricHillActivity.class);
+                    startActivity(historicAct);
+                }
+                if(menuItem.getItemId() == R.id.lakeside_crossing) {
+                    Intent lakesideAct = new Intent(getApplicationContext(), LakesideCrossingActivity.class);
+                    startActivity(lakesideAct);
+                }
+                if(menuItem.getItemId() == R.id.red_rocks) {
+                    Intent redAct = new Intent(getApplicationContext(), RedRocksActivity.class);
+                    startActivity(redAct);
+                }
+                if(menuItem.getItemId() == R.id.dining) {
+                    Intent diningAct = new Intent(getApplicationContext(), DiningActivity.class);
+                    startActivity(diningAct);
+                }
+                if(menuItem.getItemId() == R.id.map) {
+                    Intent mapAct = new Intent(getApplicationContext(), MapsActivity.class);
+                    startActivity(mapAct);
+                }
+
+                mDrawer.closeDrawers();
+                return true;
+
+            }
+        });
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.drawer, menu);
+        return true;
+    }
+
+
+    // Response from Edit Product Activity
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // if result code 100
+        if (resultCode == 100) {
+            // if result code 100 is received
+            // means user edited/deleted product
+            // reload this screen again
+            Intent intent = getIntent();
+            finish();
+            startActivity(intent);
+        }
+
+    }
+
+    /**
+     * Background Async Task to Load all product by making HTTP Request
+     * */
+    class LoadAllAnimals extends AsyncTask<String, String, String> {
+
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(LakesideCrossingActivity.this);
+            pDialog.setMessage("Loading animals. Please wait...");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(false);
+            pDialog.show();
+        }
+
+        /**
+         * getting All products from url
+         * */
+        protected String doInBackground(String... args) {
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            // getting JSON string from URL
+            JSONObject json = jParser.makeHttpRequest(url_all_animals, "GET", params);
+
+            // Check your log cat for JSON reponse
+            Log.d("Product: ", json.toString());
+
+            try {
+                // Checking for SUCCESS TAG
+                int success = json.getInt(TAG_SUCCESS);
+
+                if (success == 1) {
+                    // animals found
+                    // Getting Array of Animals
+                    animals = json.getJSONArray("animal");
+
+                    // looping through All Products
+                    for (int i = 0; i < animals.length(); i++) {
+                        JSONObject c = animals.getJSONObject(i);
+
+                        // Storing each json item in variable
+                        String id = c.getString("Animal_ID");
+                        String name = c.getString(TAG_NAME);
+
+                        // creating new HashMap
+                        HashMap<String, String> map = new HashMap<String, String>();
+
+                        // adding each child node to HashMap key => value
+                        map.put("Animal_ID", id);
+                        map.put(TAG_NAME, name);
+
+                        // adding HashList to ArrayList
+                        animalsList.add(map);
+                    }
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after getting all products
+            pDialog.dismiss();
+            // updating UI from Background Thread
+            runOnUiThread(new Runnable() {
+                public void run() {
+                    /**
+                     * Updating parsed JSON data into ListView
+                     * */
+                    ListAdapter adapter = new SimpleAdapter(
+                            LakesideCrossingActivity.this, animalsList,
+                            R.layout.list_animal, new String[] { TAG_ANIMAL_ID,
+                            TAG_NAME},
+                            new int[] { R.id.Animal_ID, R.id.Name });
+                    // updating listview
+                    setListAdapter(adapter);
+                }
+            });
+
+        }
+
+    }
+}
